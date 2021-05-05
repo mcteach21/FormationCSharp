1b5b7d75b5a4d094aa6bec5320b7e4ca551b6c9f
MC <mc69.dashboard@gmail.com>
2021-05-05 17:16:37 +0200

    EF Code First - Db First Examples

---
+++ Didacticiel/Program.cs
@@ -0,0 +1,35 @@
+﻿using Didacticiel.db;
+using System;
+
+namespace Didacticiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************************");
+            Console.WriteLine("**** Bien démarrer avec EF Core ****");
+            Console.WriteLine("************************************");
+
+            DatabaseTests();
+        }
+
+        private static void DatabaseTests()
+        {
+            var dao1 = new Dao<Blog>("Blog");
+            var dao2 = new Dao<Post>("Post");
+
+            dao1.add(new Blog("EF Core with Reflection.fr"));
+            dao1.add(new Blog("EF Core with Reflection.com"));
+
+            var post1 = new Post();
+            post1.Title = "Post EF Core Demo!";
+            post1.Content = "lorem ipsum..";
+            post1.BlogId = 1;
+
+            dao2.add(post1);
+
+            
+        }
+    }
+}
---
+++ Didacticiel/db/Dao.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Didacticiel.db
+{
+    class Dao<T> : IDao<T>
+    {
+        const string DbContextName= "BloggingContext";
+
+        string entityName;
+        Type contextType;
+        DbContext context;
+        PropertyInfo dbSetProperty;
+        Type entityType;
+
+        public Dao(string entityName) => InitContext(entityName);
+                
+        private void InitContext(string entityName)
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            
+            contextType = Type.GetType($"{currentAssembly.GetName().Name}.db.{DbContextName}");
+            context = (DbContext)Activator.CreateInstance(contextType);
+            
+            dbSetProperty = contextType.GetProperty($"{entityName}s");
+
+            this.entityName = entityName;
+            entityType = Type.GetType($"{currentAssembly.GetName().Name}.{entityName}");
+        }
+
+        public List<T> list()
+        {
+            IQueryable<T> result =  (IQueryable<T>)dbSetProperty.GetValue(context);
+            return result.ToList();
+        }
+        public T find(int id)
+        {
+            var findMethod = contextType.GetMethod("Find", new[] { typeof(Type) ,  typeof(object[]) });
+            var item = (T)findMethod.Invoke(context, 
+                new object[] {
+                    entityType , new object[] { id } 
+                });
+
+            return item;
+        }
+        public bool add(T item) => action("Add", item);
+        public bool delete(T item) => action("Remove", item);
+
+        public bool delete(int id)
+        {
+            var item = find(id);
+            if (item != null)
+                return delete(item);
+            return false;
+        }
+        public bool update(T item) => action("Update", item);
+
+        private bool action(string action, T item)
+        {
+            var actionMethod = contextType.GetMethod(action, new[] { typeof(object) });
+            actionMethod.Invoke(context, new object[] { item });
+
+            //context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Blogs] ON");
+
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
